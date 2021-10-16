0b198ae547ee1612a461dbfbf0cab429b80f9873
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2021-10-16 11:39:07 +0200

    Change variable name

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from yak_parser.StatechartParser import StatechartParser
 
 
 path = '02_light_switch.ysc'
-name_without_extension = os.path.splitext(path)[0]
+name_base = os.path.splitext(path)[0]
 statechart = StatechartParser().parse(path=path)
 
-statechart.save_hierarchy_image(name_without_extension + '.svg')
+statechart.save_hierarchy_image(name_base + '.svg')
 statechart.print_transitions()
 
 
 bryan.process(statechart)
 
 
-statechart.save_hierarchy_image(name_without_extension + '_processed.svg')
+statechart.save_hierarchy_image(name_base + '_processed.svg')
 statechart.print_transitions()
