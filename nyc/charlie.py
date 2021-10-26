97ae2c6b7ca1dbe14e27126f744e65201e53361a
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2021-10-26 15:08:06 +0200

    Complete first matching example

--- nyc/charlie.py
+++ nyc/charlie.py
@@ -1,18 +1,32 @@
 import itertools
 from collections import Counter
+from enum import Enum
 from typing import List, Tuple, Any
 
 import networkx
+from yak_parser.Statechart import Statechart, NodeType
+
+
+class MappingDirection(Enum):
+    LEFT_TO_RIGHT = 1
+    RIGHT_TO_LEFT = 2
 
 
 def score(graph1: networkx.DiGraph, graph2: networkx.DiGraph, mapping: List[Tuple[Any, Any]]) -> float:
-    return (len(matched_node_labels(graph1, graph2, mapping)) + len(matched_node_labels(graph2, graph1, mapping))) / \
-           (len(get_labeled_nodes(graph1)) + len(get_labeled_nodes(graph2)))
+    return (
+                   len(matched_node_labels(graph1, graph2, mapping, MappingDirection.LEFT_TO_RIGHT)) +
+                   len(matched_node_labels(graph2, graph1, mapping, MappingDirection.RIGHT_TO_LEFT))
+           ) / \
+           (
+                   len(get_labeled_nodes(graph1)) +
+                   len(get_labeled_nodes(graph2))
+           )
 
 
-def matched_node_labels(graph1: networkx.DiGraph, graph2: networkx.DiGraph, mapping: List[Tuple[Any, Any]]):
+def matched_node_labels(graph1: networkx.DiGraph, graph2: networkx.DiGraph, mapping: List[Tuple[Any, Any]],
+                        mapping_direction: MappingDirection):
     return [labeled_node for labeled_node in get_labeled_nodes(graph1)
-            if labeled_node_is_matched(labeled_node, mapping, graph2)]
+            if labeled_node_is_matched(labeled_node, mapping, graph2, mapping_direction)]
 
 
 def get_labeled_nodes(graph: networkx.DiGraph):
@@ -25,33 +39,27 @@ def get_labeled_nodes(graph: networkx.DiGraph):
 
 
 def labeled_node_is_matched(labeled_node: Tuple[Any, Any], mapping: List[Tuple[Any, Any]],
-                            graph: networkx.DiGraph) -> bool:
-    mapped_node = apply_mapping(mapping, labeled_node[0])
+                            graph: networkx.DiGraph, mapping_direction: MappingDirection) -> bool:
+    mapped_node = apply_mapping(labeled_node[0], mapping, mapping_direction)
     labels = [label for node, label in get_labeled_nodes(graph) if node == mapped_node]
     return labeled_node[1] in labels
 
 
-def apply_mapping(mapping: List[Tuple[Any, Any]], node: Any):
-    relevant_mapping_elements = [(x, y) for x, y in mapping if x == node or y == node]
+def apply_mapping(node: Any, mapping: List[Tuple[Any, Any]], mapping_direction: MappingDirection):
+    relevant_mapping_elements = [(x, y) for x, y in mapping if
+                                 (x if mapping_direction == MappingDirection.RIGHT_TO_LEFT else y) == node]
     if len(relevant_mapping_elements) > 1:
         raise ValueError('A very specific bad thing happened')
     elif len(relevant_mapping_elements) == 1:
-        x, y = relevant_mapping_elements[0]
-        return y if x == node else x
-
-
-graph1 = networkx.DiGraph()
-graph1.add_nodes_from([(0, {'labels': ['a']}), (1, {'labels': []})])
-graph1.add_edges_from([(0, 1)])
-
-graph2 = networkx.DiGraph()
-graph2.add_nodes_from([(2, {'labels': ['a']}), (3, {'labels': ['b', 'm']})])
-graph2.add_edges_from([(2, 3)])
+        return relevant_mapping_elements[0][1 if mapping_direction == MappingDirection.RIGHT_TO_LEFT else 0]
 
 
 def mapping_has_splits(mapping: List[list]):
-    mapping_nodes_counted = Counter([node for sublist in mapping for node in sublist])
-    return any(count > 1 for count in mapping_nodes_counted.values())
+    left_mapping_nodes_counted = Counter([x[0] for x in mapping])
+    right_mapping_nodes_counted = Counter([x[1] for x in mapping])
+    return \
+        any(count > 1 for count in left_mapping_nodes_counted.values()) or \
+        any(count > 1 for count in right_mapping_nodes_counted.values())
 
 
 def get_all_possible_mappings(graph1: networkx.DiGraph, graph2: networkx.DiGraph):
@@ -65,9 +73,21 @@ def get_all_possible_mappings(graph1: networkx.DiGraph, graph2: networkx.DiGraph
     return [[(x[0], x[1]) for x in mapping] for mapping in all_possible_mappings_list]
 
 
-possible_mappings = get_all_possible_mappings(graph1, graph2)
-print(max(possible_mappings, key=lambda mapping: score(graph1, graph2, mapping)))
+def create_comparison_graph(statechart: Statechart):
+    graph = networkx.DiGraph()
+    for node in [
+        node for node in statechart.hierarchy.nodes if statechart.hierarchy.nodes[node]['ntype'] == NodeType.STATE
+    ]:
+        graph.add_node(node, labels=['state'])
+    return graph
 
 
-def noji() -> List[str]:
-    return ['s']
+def get_additions(statechart1: Statechart, statechart2: Statechart) -> List[str]:
+    graph1 = create_comparison_graph(statechart1)
+    graph2 = create_comparison_graph(statechart2)
+
+    possible_mappings = get_all_possible_mappings(graph1, graph2)
+    best_mapping = max(possible_mappings, key=lambda mapping: score(graph1, graph2, mapping))
+
+    added_nodes = [node for node in graph2.nodes if node not in [y for x, y in best_mapping]]
+    return [statechart2.hierarchy.nodes[node]['obj'].name for node in added_nodes]
---
+++ tests/test_matching.py
@@ -0,0 +1,20 @@
+# Allow direct execution
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # noqa: E402
+
+import unittest
+
+from nyc import charlie
+from yak_parser.StatechartParser import StatechartParser
+
+
+class TestMatching(unittest.TestCase):
+    def test(self):
+        statechart1 = StatechartParser().parse(path='testdata/test_matching/test1.ysc')
+        statechart2 = StatechartParser().parse(path='testdata/test_matching/test2.ysc')
+        self.assertEqual(['Mid'], charlie.get_additions(statechart1, statechart2))
+
+
+if __name__ == '__main__':
+    unittest.main()
