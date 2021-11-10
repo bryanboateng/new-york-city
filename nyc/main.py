b97c0f178f125a98b8b5a6bc6b06d43c5f75316e
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2021-11-11 00:04:48 +0100

    Create command that shows matches

--- nyc/main.py
+++ nyc/main.py
@@ -23,9 +23,10 @@ class Main:
             Commands:
                compare     Compare statecharts
                list        List found cases of plagiarism
+               matches     Show matches
             '''
         )
-        parser.add_argument('command', help='Subcommand to run', choices=['compare', 'list'])
+        parser.add_argument('command', help='Subcommand to run', choices=['compare', 'list', 'matches'])
         args = parser.parse_args(sys.argv[1:2])
         if not hasattr(self, args.command):
             print('Unrecognized command')
@@ -56,9 +57,7 @@ class Main:
     def list():
         parser = argparse.ArgumentParser(description='List found cases of plagiarism')
         parser.add_argument('result_file', help="Path of the comparison result file")
-        result_file = open(parser.parse_args(sys.argv[2:]).result_file, 'rb')
-        new_dict = pickle.load(result_file)
-        result_file.close()
+        comparison_result = Main.load_comparison_result(parser.parse_args(sys.argv[2:]).result_file)
         table = [
             [
                 (Fore.GREEN + f"#{i}"),
@@ -67,10 +66,23 @@ class Main:
                 "{:.2%}".format(result.similarity),
                 "{:.2%}".format(result.max_similarity)
             ]
-            for i, (path1, path2, result) in enumerate(new_dict, start=1)
+            for i, (path1, path2, result) in enumerate(comparison_result, start=1)
             if result.similarity > 0.8 or result.max_similarity > 0.8
         ]
         print(tabulate(table, headers=['ID', 'File 1', 'File 2', 'Average similarity', 'Maximum similarity']))
+
+    @staticmethod
+    def matches():
+        parser = argparse.ArgumentParser(description='Show matches')
+        parser.add_argument('result_file', help="Path of the comparison result file")
+        parser.add_argument('id', type=int, help="ID")
+        arguments = parser.parse_args(sys.argv[2:])
+        comparison_result = Main.load_comparison_result(arguments.result_file)
+        path1, path2, comparison_result_ = comparison_result[arguments.id - 1]
+        print(f'Average similarity: {"{:.2%}".format(comparison_result_.similarity)}')
+        print(f'Maximum similarity: {"{:.2%}".format(comparison_result_.max_similarity)}')
+        for (id1, id2), labels in comparison_result_.diff.matches.items():
+            print(f'{id1} - {id2}: {labels}')
 
     @staticmethod
     def load_statecharts(directory):
@@ -88,6 +100,13 @@ class Main:
         pickle.dump(comparison_result, result_file)
         result_file.close()
         print(f"Result saved as {result_filename}")
+
+    @staticmethod
+    def load_comparison_result(path):
+        result_file = open(path, 'rb')
+        comparison_result = pickle.load(result_file)
+        result_file.close()
+        return comparison_result
 
 
 if __name__ == '__main__':
